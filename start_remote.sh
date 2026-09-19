2ff887e3a257f0f9df7a1e99fb38fa831af833ab
alameenahmed318-bit <alameenahmed318@gmail.com>
2026-09-19 18:56:26 +0400

    Add Railway remote browser startup

---
+++ start_remote.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+: "${REMOTE_LOGIN_PASSWORD:?Set REMOTE_LOGIN_PASSWORD in Railway Variables}"
+export DISPLAY=:99
+Xvfb :99 -screen 0 1400x900x24 -ac &
+sleep 2
+x11vnc -display :99 -forever -shared -rfbport 5900 -nopw -localhost &
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
+python -u remote_login.py &
+exec python -u demo_connection_test.py
